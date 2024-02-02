9dea062868cb372194981f256a0593b0983d3ea5
Paulo Lopes <paulohdas.lopes@gmail.com>
2024-02-02 09:26:20 -0300

    :construction: created header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import logo from '../../logo.svg';
+import logo from '../../resources/logo.svg';
+import icon from '../../resources/in.svg';
+
 import './styles.css'
 
 export default function Header(){
     return (
         <header className="header">
-            <img src={logo} alt="" className="logo"/>
+            <img src={logo} alt="app logo" className="logo"/>
             <h1 className="title">Meu App</h1>
-            <a href="" className="link">Linkedin</a>
+            <a href="https://www.linkedin.com/in/paulo-coimbra-b1152717b/" className="link">
+                <img src={icon} alt="linkedin icon" className="logo"/>
+            </a>
         </header>
     )
 }
